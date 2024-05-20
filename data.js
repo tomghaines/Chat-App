403dcb4e08c825de8b7449b95549f406a9ba9689
tomghaines <tomhaines001@gmail.com>
2024-05-20 23:02:06 +0100

    Finished first draft

---
+++ data.js
@@ -0,0 +1,36 @@
+export function getDate() {
+    const date = new Date()
+
+    const dateOptions = {
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric'
+    }
+    
+const timeOptions = {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    }
+    
+const displayTime = date.toLocaleTimeString('en-UK', timeOptions)
+const displayDate = date.toLocaleDateString('en-UK', dateOptions)
+
+return {
+    displayDate: displayDate,
+    displayTime: displayTime
+}
+}
+
+export const responses = [
+    "That's interesting! Tell me more.",
+    "I didn't know that. Thanks for sharing!",
+    "Can you explain that a bit further?",
+    "Wow, that sounds amazing!",
+    "I'm not sure I understand. Could you clarify?",
+    "Did you know that honey never spoils? Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still perfectly edible.",
+    "Bananas are berries, but strawberries aren’t. Botanically speaking, a berry is a fruit produced from the ovary of a single flower with seeds embedded in the flesh.",
+    "The shortest war in history was between Britain and Zanzibar on August 27, 1896. Zanzibar surrendered after 38 minutes.",
+    "Octopuses have three hearts. Two pump blood to the gills, while the third pumps it to the rest of the body.",
+    "The Eiffel Tower can be 15 cm taller during the summer due to the expansion of iron when it heats up."
+]
---
+++ script.js
@@ -0,0 +1,84 @@
+import { getDate, responses } from "./data.js"
+
+const inputEl = document.getElementById('text-input')
+const submitBtn = document.getElementById('submit-btn')
+const chatField = document.getElementById('chat-display')
+
+function getReply() {
+    let reply = ''
+    let randomNum = Math.floor(Math.random() * responses.length)
+    reply = responses[randomNum]
+    return reply
+}
+
+function receiveMessage() {
+
+    let currentDate = getDate()
+
+    let displayDate = currentDate.displayDate
+    let displayTime = currentDate.displayTime
+
+    let output = getReply()
+
+    const incomingMessage = {
+        sender: 'Danny',
+        message: output,
+        displayDate: displayDate,
+        displayTime: displayTime
+    }
+
+    chatField.innerHTML += newChatTwo(incomingMessage)
+    chatField.scrollTop = chatField.scrollHeight
+}
+
+function sendMessage() {
+
+    let currentDate = getDate()
+
+    let displayDate = currentDate.displayDate
+    let displayTime = currentDate.displayTime
+
+    if(inputEl.value === '') {
+        return
+    }
+
+    const outgoingMessage = {
+        sender: 'You',
+        message: inputEl.value,
+        displayDate: displayDate,
+        displayTime: displayTime
+    }
+
+    chatField.innerHTML += newChat(outgoingMessage)
+    chatField.scrollTop = chatField.scrollHeight
+
+    inputEl.value = ''
+
+    setTimeout(receiveMessage, 1000)
+}
+
+function newChat(outgoingMessage) {
+    return `
+        <div class="message-bubble" id="message-bubble">
+            <p class="sender" id="sender">${outgoingMessage.sender}</p>
+            <p class="message-content" id="message-content">${outgoingMessage.message}</p>
+            <div class="date-time" id="date-time">
+                <p class="date" id="date">${outgoingMessage.displayDate}</p>
+                <p class="time" id="time">${outgoingMessage.displayTime}</p>
+            <div>
+        </div>`
+}
+
+function newChatTwo(incomingMessage) {
+    return `
+        <div class="message-bubble-incoming" id="message-bubble-incoming">
+            <p class="sender" id="sender">${incomingMessage.sender}</p>
+            <p class="message-content" id="message-content">${incomingMessage.message}</p>
+            <div class="date-time" id="date-time">
+                <p class="date" id="date">${incomingMessage.displayDate}</p>
+                <p class="time" id="time">${incomingMessage.displayTime}</p>
+            <div>
+        </div>`
+}
+
+submitBtn.addEventListener('click', sendMessage)
